0e5b0525daa2e25bfe988605b9921e64e0243fa7
HussAlbaiti <160526382+HussAlbaiti@users.noreply.github.com>
2025-05-25 13:56:55 +0300

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,11 +1,7 @@
 const firstArgument = process.argv[2];
 
 if (firstArgument) {
-<<<<<<< HEAD
-    console.log(firstArgument);
-=======
     console.log(firstArgument)
->>>>>>> 912d95c0e5565ac9c5ea800c81656c5221d39574
 }
 else {
     console.log('No argument');
